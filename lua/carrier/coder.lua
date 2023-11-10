e1fe0815d41465bbe4649091c788442d7f74d347
Camden <camdenaws@gmail.com>
2023-11-09 19:23:06 -0800

    getting closer to representing code in the buffer

--- lua/carrier/coder.lua
+++ lua/carrier/coder.lua
@@ -1,3 +1,5 @@
+local openai = require("carrier.openai")
+
 local HEAD = "<<<<<<< SEARCH"
 local DIVIDER = "======="
 local UPDATED = ">>>>>>> REPLACE"
@@ -9,6 +11,34 @@ local function find_update_block(content)
 
     return { filename, search, replace }
 end
+
+local function replace_in_buffer(bufnr, update_block)
+    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    local _, search, replace = unpack(update_block)
+    local contents = table.concat(lines, "\n")
+
+    contents = contents:gsub(search, replace)
+
+    -- Split the contents back into lines
+    local new_lines = {}
+    for line in contents:gmatch("([^\n]*)\n?") do
+        table.insert(new_lines, line)
+    end
+
+    -- Set the lines in the buffer
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, new_lines)
+end
+
+local function replace_in_buffer_by_filename(filename, update_block)
+    local bufnr = vim.fn.bufnr(filename)
+    if bufnr == -1 then
+        return
+    end
+    replace_in_buffer(bufnr, update_block)
+end
+
+local function get_edit_block() end
+
 local edit = [[
 Heres the change:
 
